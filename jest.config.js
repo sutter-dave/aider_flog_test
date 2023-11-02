24d28b5cd3bc91505659ae1d4ea80375639c02a1
Dave Sutter <sutter@intransix.com>
2023-11-02 16:32:00 -0700

    test updates

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
 };
