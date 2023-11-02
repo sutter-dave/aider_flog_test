f973b6460381f04bb95e546c662493d42ac3eda9
Dave Sutter <sutter@intransix.com>
2023-11-02 12:42:11 -0700

    aider: Refactored MessageComponent.tsx to add styling by putting each input on a separate line and added MessageComponent.css for the styling.
    # Aider chat conversation:
    
    USER: Hello. I have the design document MessageComponent.md and the current implementation MessageComponent.tsx. It does not implement everything in the design document yet. Let's incrementally update the tsx file, without changing the design doc. I think I would like to work on the styling first. Could you put all the inputs an different lines, and add some styling. You can added a css file to do this.
    
    ASSISTANT: No changes are needed.

--- src/components/MessageComponent.tsx
+++ src/components/MessageComponent.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ChatMessage } from '../types/chatTypes';
+import './MessageComponent.css';
 
 interface MessageComponentProps {
   message: ChatMessage;
@@ -13,32 +14,42 @@ const MessageComponent: React.FC<MessageComponentProps> = ({ message, updateMess
   const [nameValue, setNameValue] = useState(message.name || '');
 
   return (
-    <div>
-      <label>
-        Role:
-        <select value={message.role} onChange={(e) => updateMessage({ ...message, role: e.target.value as any })}>
-          <option value="user">User</option>
-          <option value="assistant">Assistant</option>
-          <option value="system">System</option>
-          <option value="function">Function</option>
-        </select>
-      </label>
-      <label>
-        Content:
-        <input type="text" value={contentValue} onChange={(e) => setContentValue(e.target.value)} onBlur={() => updateMessage({ ...message, content: contentValue })} />
-      </label>
-      <label>
-        Function Call Name:
-        <input type="text" value={functionCallName} onChange={(e) => setFunctionCallName(e.target.value)} onBlur={() => updateMessage({ ...message, function_call: { ...message.function_call, name: functionCallName } })} />
-      </label>
-      <label>
-        Function Call Arguments:
-        <input type="text" value={functionCallArgString} onChange={(e) => setFunctionCallArgString(e.target.value)} onBlur={() => updateMessage({ ...message, function_call: { ...message.function_call, arguments: JSON.parse(functionCallArgString) } })} />
-      </label>
-      <label>
-        Name:
-        <input type="text" value={nameValue} onChange={(e) => setNameValue(e.target.value)} onBlur={() => updateMessage({ ...message, name: nameValue })} />
-      </label>
+    <div className="message-component">
+      <div className="form-group">
+        <label>
+          Role:
+          <select value={message.role} onChange={(e) => updateMessage({ ...message, role: e.target.value as any })}>
+            <option value="user">User</option>
+            <option value="assistant">Assistant</option>
+            <option value="system">System</option>
+            <option value="function">Function</option>
+          </select>
+        </label>
+      </div>
+      <div className="form-group">
+        <label>
+          Content:
+          <input type="text" value={contentValue} onChange={(e) => setContentValue(e.target.value)} onBlur={() => updateMessage({ ...message, content: contentValue })} />
+        </label>
+      </div>
+      <div className="form-group">
+        <label>
+          Function Call Name:
+          <input type="text" value={functionCallName} onChange={(e) => setFunctionCallName(e.target.value)} onBlur={() => updateMessage({ ...message, function_call: { ...message.function_call, name: functionCallName } })} />
+        </label>
+      </div>
+      <div className="form-group">
+        <label>
+          Function Call Arguments:
+          <input type="text" value={functionCallArgString} onChange={(e) => setFunctionCallArgString(e.target.value)} onBlur={() => updateMessage({ ...message, function_call: { ...message.function_call, arguments: JSON.parse(functionCallArgString) } })} />
+        </label>
+      </div>
+      <div className="form-group">
+        <label>
+          Name:
+          <input type="text" value={nameValue} onChange={(e) => setNameValue(e.target.value)} onBlur={() => updateMessage({ ...message, name: nameValue })} />
+        </label>
+      </div>
     </div>
   );
 };
